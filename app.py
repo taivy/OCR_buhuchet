73d55cf20ee1d2a150b9c190e5050423cfd76b68
taivy <aie803ef4g@gmail.com>
2019-05-19 17:34:08 +0300

    comments added

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template, request
 from pdf2image import convert_from_bytes
 import io
 from itertools import chain
+import os
 
-from yandex_ocr_request import get_yandex_cloud_ocr_response
+from yandex_ocr_request_func import get_yandex_cloud_ocr_response
 from ocr_funcs import ocr_buhuchet
 from crop import crop_frames
 
 
+SAVE_IMAGES_MODE = True
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'bmp'])
 
 app = Flask(__name__)
@@ -22,6 +24,12 @@ def main():
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
+        if SAVE_IMAGES_MODE:
+            # filename wihtout extension
+            filename = f.filename[:f.filename.find('.')]
+            file_images_dir = os.path.join('images', filename)
+            if not os.path.exists(file_images_dir):
+                os.mkdir(file_images_dir)
         if f.mimetype.endswith('pdf'):
             pages = convert_from_bytes(f.read())
             result = []
@@ -31,13 +39,25 @@ def upload_file():
                 imgByteArr = io.BytesIO()
                 cropped_page.save(imgByteArr, format='PNG')
                 resp = get_yandex_cloud_ocr_response(imgByteArr.getvalue())
+                if SAVE_IMAGES_MODE:
+                    image_path = os.path.join(file_images_dir, "%s_image.jpg" % str(i))
+                    cropped_page.save(image_path, "JPEG") 
+                    response_path = os.path.join(file_images_dir, "%s_response.json" % str(i))
+                    with open(response_path, 'w') as outf:
+                        outf.write(resp)
                 r = ocr_buhuchet(resp, debug_mode=False)
                 result = list(chain(result, [r]))
                 i+=1                
         else:
             image_data = f.read()
-        
             resp = get_yandex_cloud_ocr_response(image_data)
+            if SAVE_IMAGES_MODE:
+                image_path = os.path.join(file_images_dir, "image.jpg")
+                with open(image_path, 'wb') as outf:
+                    outf.write(image_data)
+                response_path = os.path.join(file_images_dir, "response.json")
+                with open(response_path, 'w') as outf:
+                    outf.write(resp)
             r = ocr_buhuchet(resp)
             result = r
         return render_template('recognized.html', result=result)
